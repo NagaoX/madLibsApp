8f3adce44e409459b59cc4181e38df1c845155aa
NagaoX <106872297+NagaoX@users.noreply.github.com>
2022-07-10 18:42:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ let userInputs = [];
 
 let numberOfQuestions = 6
 
-let questionArray = [prompt(`Enter a day of the week:`), prompt(`Enter a color:`), prompt(`Enter an animal:`), prompt(`Enter a verb:`), prompt(`Enter a country`), prompt(`Enter an adjective:`)];
-
-for (let i = numberOfQuestions; i >= 0; i--) {
-  //console.log(i);
-}
+let questionArray = [`Enter a day of the week:`, `Enter a color:`, `Enter an animal:`, `Enter a verb:`, `Enter a country:`, `Enter an adjective:`];
 
 let questionCounter = 0;
 let questionTotal = 6;
 for (let i = questionCounter; i < numberOfQuestions; i++) {
   console.log(questionArray[i] + ` (${questionTotal} questions left)`)
+  userInputs.push(prompt(questionArray[i] + ` (${questionTotal} questions left)`))
   questionTotal--;
 }
+
+console.log('All done! Ready for your mad-lib story??');
 
 let originalStory = `${userInputs[0]} we went to the Zoo. The first thing we saw was a ${userInputs[1]} ${userInputs[2]} ${userInputs[3]}ing. The zookeeper told us that was normal, except in ${userInputs[4]}. I had a ${userInputs[5]} time. Next time, I will remeber that if I ever see a ${userInputs[1]} ${userInputs[2]}, I should ${userInputs[6]} the other way.`;
 
