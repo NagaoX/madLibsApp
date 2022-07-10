c81e56ed6387257d3743ffc0f52a8dca46acabc5
NagaoX <106872297+NagaoX@users.noreply.github.com>
2022-07-10 17:43:36 -0300

    Update app.js
    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let originalStory = `${userInputs[0]} we went to the Zoo. The first thing we saw
 
 let userInputs = [];
 
+let numberOfQuestions = userInputs.lenght
+
 /*
 originalStory = `(dayOfTheWeek) we went to the Zoo. The first thing we saw was a (color) (animal) (verb)ing. The zookeeper told us that was normal,except in (country). I had a (adjective) time. Next time, I will remeber that if I ever see a (color) (animal), I should (verb) the other way.`;
-*/    
+*/ 
