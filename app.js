26910a7d03faf56d3c02678e153b7d4039419306
NagaoX <106872297+NagaoX@users.noreply.github.com>
2022-07-10 18:32:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let originalStory = `${userInputs[0]} we went to the Zoo. The first thing we saw was a ${userInputs[1]} ${userInputs[2]} ${userInputs[3]}ing. The zookeeper told us that was normal, except in ${userInputs[4]}. I had a ${userInputs[5]} time. Next time, I will remeber that if I ever see a ${userInputs[1]} ${userInputs[2]}, I should ${userInputs[6]} the other way.`;
-
 let userInputs = [];
 
 let numberOfQuestions = 6
@@ -7,13 +5,17 @@ let numberOfQuestions = 6
 let questionArray = [prompt(`Enter a day of the week:`), prompt(`Enter a color:`), prompt(`Enter an animal:`), prompt(`Enter a verb:`), prompt(`Enter a country`), prompt(`Enter an adjective:`)];
 
 for (let i = numberOfQuestions; i >= 0; i--) {
-  console.log(i);
+  //console.log(i);
 }
 
 let questionCounter = 0;
+let questionTotal = 6;
 for (let i = questionCounter; i < numberOfQuestions; i++) {
-  console.log(questionArray[i]);
+  console.log(questionArray[i] + ` (${questionTotal} questions left)`)
+  questionTotal--;
 }
+
+let originalStory = `${userInputs[0]} we went to the Zoo. The first thing we saw was a ${userInputs[1]} ${userInputs[2]} ${userInputs[3]}ing. The zookeeper told us that was normal, except in ${userInputs[4]}. I had a ${userInputs[5]} time. Next time, I will remeber that if I ever see a ${userInputs[1]} ${userInputs[2]}, I should ${userInputs[6]} the other way.`;
 
 /*
 originalStory = `(dayOfTheWeek) we went to the Zoo. The first thing we saw was a (color) (animal) (verb)ing. The zookeeper told us that was normal,except in (country). I had a (adjective) time. Next time, I will remeber that if I ever see a (color) (animal), I should (verb) the other way.`;
