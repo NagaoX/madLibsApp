43c1b5408b65f27ea01ae9b33b53b7a28c4ffb07
NagaoX <106872297+NagaoX@users.noreply.github.com>
2022-07-10 17:39:35 -0300

    Create app.js
    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,7 @@
+let originalStory = `${userInputs[0]} we went to the Zoo. The first thing we saw was a ${userInputs[1]} ${userInputs[2]} ${userInputs[3]}ing. The zookeeper told us that was normal, except in ${userInputs[4]}. I had a ${userInputs[5]} time. Next time, I will remeber that if I ever see a ${userInputs[1]} ${userInputs[2]}, I should ${userInputs[6]} the other way.`;
+
+let userInputs = [];
+
+/*
+originalStory = `(dayOfTheWeek) we went to the Zoo. The first thing we saw was a (color) (animal) (verb)ing. The zookeeper told us that was normal,except in (country). I had a (adjective) time. Next time, I will remeber that if I ever see a (color) (animal), I should (verb) the other way.`;
+*/    
